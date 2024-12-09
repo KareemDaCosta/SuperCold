f11c238a7e63437a8d9c5815d0882dd2c8d1a108
Steven Huang <96713218+huangs-nyc@users.noreply.github.com>
2024-12-08 23:34:14 -0500

    Add files via upload

---
+++ src/Arduino/SuperCold/supercold-hardware-comm-final.ino
@@ -0,0 +1,125 @@
+#include <WiFi.h>
+#include <esp_now.h>
+
+#define BUTTON_PIN 21
+
+int xyzPins[] = {36, 37, 38};   // x, y, z(switch) pins first one
+int xyzPins2[] = {39, 32, 33}; // x, y, z (switch) pins second one
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(xyzPins[2], INPUT_PULLUP);  // pullup resistor for switch
+  pinMode(xyzPins2[2], INPUT_PULLUP);  // pullup resistor for switch
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // for button
+  espnowSetup();
+}
+
+void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength)
+// Formats MAC Address
+{
+  snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+}
+
+void receiveCallback(const esp_now_recv_info_t *macAddr, const uint8_t *data, int dataLen)
+/* Called when data is received
+   You can receive 3 types of messages
+   1) a "POSITION" message, which indicates the opponent's position (x, y), velocity (x, y), and camera angle.
+   2) a "FIRE" message, which indicates the opponent has fired their gun (and whether they hit)
+   3) a "SETUP" message, which is sent once at startup and sends the map information and the joiner's position.
+   4) a "SETUP COMPLETE", which is sent once when the joiner completes their setup.
+   
+   Messages are formatted as follows:
+   [P/F/S/C]: message
+   For example, an POSITION message for "x: 15, y: 15, velocityX: 13, velocityY: 13, angle: 90":
+   P: 15,15,13,13,90
+   For example, a FIRE message with success false:
+   F: false
+*/
+
+{
+  // Only allow a maximum of 250 characters in the message + a null terminating byte
+  char buffer[ESP_NOW_MAX_DATA_LEN + 1];
+  int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
+  strncpy(buffer, (const char *)data, msgLen);
+
+  // Make sure we are null terminated
+  buffer[msgLen] = 0;
+  String recvd = String(buffer);
+  // Format the MAC address
+  char macStr[18];
+  // formatMacAddress(macAddr, macStr, 18);
+
+  // Send message to the serial port
+  Serial.println(recvd);
+}
+
+void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status)
+// Called when data is sent
+{
+  char macStr[18];
+  formatMacAddress(macAddr, macStr, 18);
+}
+
+void broadcast(const String &message)
+// Emulates a broadcast
+{
+  // Broadcast a message to every device in range
+  uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(&peerInfo.peer_addr, broadcastAddress, 6);
+  if (!esp_now_is_peer_exist(broadcastAddress)) {
+    esp_now_add_peer(&peerInfo);
+  }
+  // Send message
+  esp_err_t result = esp_now_send(broadcastAddress, (const uint8_t *)message.c_str(), message.length());
+}
+
+void espnowSetup() {
+  // Set ESP32 in STA mode to begin with
+  delay(500);
+  WiFi.mode(WIFI_STA);
+
+  // Disconnect from WiFi
+  WiFi.disconnect();
+
+  // Initialize ESP-NOW
+  if (esp_now_init() == ESP_OK) {
+    esp_now_register_recv_cb(receiveCallback);
+    esp_now_register_send_cb(sentCallback);
+  } else {
+    delay(3000);
+    ESP.restart();
+  }
+}
+
+
+void loop() {
+  int buttonState = digitalRead(BUTTON_PIN);
+  int xVal = analogRead(xyzPins[0]);
+  int yVal = analogRead(xyzPins[1]);
+  int zVal = digitalRead(xyzPins[2]);
+  int xVal2 = analogRead(xyzPins2[0]);
+  int yVal2 = analogRead(xyzPins2[1]);
+  int zVal2 = digitalRead(xyzPins2[2]);
+
+  Serial.print(xVal);
+  Serial.print(",");
+  Serial.print(yVal);
+  Serial.print(",");
+  Serial.print(zVal);
+  Serial.print(",");
+  Serial.print(xVal2);
+  Serial.print(",");
+  Serial.print(yVal2);
+  Serial.print(",");
+  Serial.print(zVal2);
+  Serial.print(",");
+  Serial.print(buttonState);
+
+  if(Serial.available() > 0) {
+    while(Serial.available() > 0) {
+      String message = Serial.readStringUntil('\n');
+      broadcast(message);
+    }
+  }
+}
